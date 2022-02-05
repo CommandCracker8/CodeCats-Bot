20bde860cbdf9aede9892fc626607128d24c9ccf
CommandCracker8 <68102222+CommandCracker8@users.noreply.github.com>
2022-02-05 20:41:22 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-// const fetch = require('node-fetch')
-
-// fetch('https://api.opensea.io/api/v1/collection/codecats/stats')
-//     .then(res => res.json())
-//     .then(json => json.stats)
-//     .then(json => {
-//         console.log(json)
-// })
-
 const Discord = require('discord.js');
 const fetch = require('node-fetch')
 const path = require('path')
 const { getEthPriceNow }= require('get-eth-price');
 var Datastore = require('nedb');
 const { exit } = require('process');
+require('dotenv').config()
 
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
 const historyDB = new Datastore({ filename: path.join(__dirname, 'history.db'), autoload: true });
-const usersToNotifyDB = new Datastore({ filename: path.join(__dirname, 'users.db'), autoload: true });
 
-function getdata(callback) {
+function fetchData(callback) {
     fetch('https://api.opensea.io/api/v1/collection/codecats/stats')
         .then(res => res.json())
         .then(json => json.stats)
@@ -28,30 +19,34 @@ function getdata(callback) {
 
             getEthPriceNow()
                 .then( data => {
-                    console.log(Object.values(data)[0].ETH.GBP)
+                    console.log(data)
+                    console.log(`Current price of ETH in GBP: £${Object.values(data)[0].ETH.GBP}`)
+                    console.log(`Current price of ETH in USD: £${Object.values(data)[0].ETH.USD}`)
 
-                    const timeString = new Date(Date.now()).toString()
-                    const costPounds = jsonStats.thirty_day_average_price * Object.values(data)[0].ETH.GBP
+                    const currentTime = new Date(Date.now()).toString()
+                    const pricePounds = jsonStats.thirty_day_average_price * Object.values(data)[0].ETH.GBP
+                    const priceDollars = jsonStats.thirty_day_average_price * Object.values(data)[0].ETH.USD
 
                     const embed = new Discord.MessageEmbed()
                         .setTitle('CodeCats Stats')
                         .setURL('https://opensea.io/collection/codecats?tab=activity')
                         .setTimestamp(Date.now())
                         .addField("Average Sale Price (30 Days)", jsonStats.thirty_day_average_price.toString() + " ETH", true)
-                        .addField("Average Sale Price (30 Days)", costPounds + " GBP", true)
+                        .addField("Average Sale Price (30 Days)", pricePounds + " GBP", true)
+                        .addField("Average Sale Price (30 Days)", priceDollars + " USD", true)
                         .addField("Number Of Owners", jsonStats.num_owners.toString(), true)
                         .addField("Average Price", jsonStats.average_price + " ETH", true)
                         .addField("Total Sales", jsonStats.total_sales.toString(), true)
-                        .addField("Date/Time", timeString)
+                        .addField("Date/Time", currentTime)
 
                     historyDB.insert({
                         timestamp: Date.now(),
                         price: {
                             eth: jsonStats.thirty_day_average_price,
-                            gbp: costPounds
+                            gbp: pricePounds
                         },
                         stats: jsonStats,
-                        timestampString: timeString
+                        timestampString: currentTime
                     }, (err, newdoc) => {
                         
                     })
@@ -64,11 +59,9 @@ function getdata(callback) {
 if (process.argv.slice(2)[0] == "--send-message") {
     client.on('ready', () => {
         console.log(`Logged in as ${client.user.tag}!`);
-    
-        // console.log(process.argv.slice(2)[0] == "--send-message")
-        
+            
         if (process.argv.slice(2)[0] == "--send-message") {
-            getdata(embed => {
+            fetchData(embed => {
                 client.users.fetch('703303356361212014', false).then((user) => {
                     user.send({ embeds: [ embed ] }).then(message => {
                         exit(0)
@@ -82,7 +75,7 @@ if (process.argv.slice(2)[0] == "--send-message") {
         console.log(`${message.author.tag} sent message "${message.content}"`)
     });
     
-    client.login('ODM2OTg5ODAwNzU3MzI5OTQw.YImBfw.x0jkvI9XoYM-k3Fpig48LaHP0lg');
+    client.login(process.env.BOT_TOKEN);
 } else {
-    getdata(() => {})
+    fetchData(() => {})
 }
